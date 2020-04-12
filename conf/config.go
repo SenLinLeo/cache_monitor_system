99240c66aca81a0a55cb793872f4f6543912fb36
SenLinLeo <45943833+SenLinLeo@users.noreply.github.com>
2020-04-12 13:50:18 +0800

    Update config.go

--- conf/config.go
+++ conf/config.go
@@ -1,12 +1,11 @@
 package conf
 
-var CodisServerUrl    = "http://172.30.2.181:18080/api/topom/group/info/"
-//var CodisServerUrl = "http://172.30.2.181:18080/api/topom/group/info/172.30.2.182:6380"
-var DashboardUrl      = "http://172.30.2.181:18080/topom"
+var CodisServerUrl    = "http://192.168.2.181:18080/api/topom/group/info/"
+var DashboardUrl      = "http://192.168.2.181:18080/topom"
 
 var MetricPath string = "/metrics"
 var Addrs             = []string{"172.30.2.182:6380"}
-var Passwords         = []string{"iboxpay"}
+var Passwords         = []string{"passwad"}
 var Aliases           = []string{"test"}
 
 
